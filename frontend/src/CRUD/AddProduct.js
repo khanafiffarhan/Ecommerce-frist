ef527213d20bc949a83b1deb3c34ef96bf22e6ee
khanafiffarhan <khanafiffarhan@gmail.com>
2022-01-26 15:35:24 +0600

    designing add product

--- frontend/src/CRUD/AddProduct.js
+++ frontend/src/CRUD/AddProduct.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddProduct = () => {
+
+  const [id, setId] = useState('');
+  const [imgsrc, setImgsrc] = useState('');
+  const [title, setTitle] = useState('');
+  const [detail, setDetail] = useState('');
+  const [prise, setPrise] = useState('');
+  const [slug, setSlug] = useState('');
+
+  function addproduct() {
+    var product = {
+      id: id,
+      imgsrc: imgsrc,
+      title: title,
+      detail: detail,
+      prise: prise,
+      slug: slug
+    }
+    console.log(product)
+
+  }
   return <div className='wrap_container'>
-     <form>
-  <label for="fname">First name:</label>
-  <input type="text" id="fname" name="fname" />
-  <label for="lname">Last name:</label>
-  <input type="text" id="lname" name="lname" />
-   <input type="submit" value="Submit" />
-</form>
+    <form>
+      <input type="number" placeholder='id' onChange={(e) => { setId(e.target.value) }} />
+      <input type="text" placeholder='imgsrc' onChange={(e) => { setImgsrc(e.target.value) }} />
+      <input type="text" placeholder='title' onChange={(e) => { setTitle(e.target.value) }} />
+      <input type="text" placeholder='detail' onChange={(e) => { setDetail(e.target.value) }} />
+      <input type="number" placeholder='price' onChange={(e) => { setPrise(e.target.value) }} />
+      <input type="text" placeholder='slug' onChange={(e) => { setSlug(e.target.value) }} />
+
+      <button onClick={addproduct} className="btn btn-success">Add Post</button>
+    </form>
   </div>;
 };
 
